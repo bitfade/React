702dab187198fa7c8cbbdfcb16379a05c7a48ad3
bitfade <bitfade@bitfade.com>
2017-07-11 12:17:42 +0200

    work

--- src/js/init.js
+++ src/js/init.js
@@ -1,14 +1,27 @@
 'use strict'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 if (module.hot) {
   module.hot.accept()
 }
 
 class Button extends React.Component {
-  state: { counter: 0 };
+  state = { counter: 0 };
 
   handleClick = () => {
     this.setState({
       counter: this.state.counter + 1
-    });
+    })
   };
+
+  render () {
+    return (
+      <button onClick={this.handleClick} >
+        {this.state.counter}
+      </button>
+    )
+  }
 }
+
+ReactDOM.render(<Button />, mountNode)
