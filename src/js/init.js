7eb856e0d22754ade79ca8d5f00fe79f087ff4b9
bitfade <bitfade@bitfade.com>
2017-07-12 15:01:10 +0200

    work

--- src/js/init.js
+++ src/js/init.js
@@ -1,17 +1,9 @@
 'use strict'
-import React from 'react'
-import ReactDOM from 'react-dom'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
 
 if (module.hot) {
   module.hot.accept()
 }
 
-const Card = (props) => {
-  return (
-    <div>
-      <img />
-    </div>
-  )
-}
-
-ReactDOM.render(<App />, mountNode)
+console.log('sippa')
